fd3bf986baa3337176bebea7829d96d7ce5b7b0d
petrashovdaniill-dotcom <petrashov.daniill@gmail.com>
2026-07-21 18:38:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -145,7 +145,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         msg = await query.message.reply_text(
             "📍 Наш адрес:\n"
-            "Укажите адрес шиномонтажа"
+            "Семипалатинская 17"
         )
 
         context.user_data["info_msg"] = msg.message_id
