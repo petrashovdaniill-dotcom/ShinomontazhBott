64e5db33b3cf7e46bbf48202014d90c535a311aa
petrashovdaniill-dotcom <petrashov.daniill@gmail.com>
2026-07-21 18:15:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,13 +120,15 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     elif query.data == "price":
 
-        await query.message.reply_text(
+        msg = await query.message.reply_text(
             "💰 Наш прайс:\n\n"
             "🚗 R14 — 1000₽\n"
             "🚙 R15-R16 — 1500₽\n"
             "🚘 R17-R18 — 2000₽\n\n"
             "Балансировка входит в стоимость."
         )
+
+        context.user_data["info_msg"] = msg.message_id
 
     elif query.data == "address":
 
