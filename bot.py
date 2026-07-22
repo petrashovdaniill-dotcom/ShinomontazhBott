57559dc9908548e6bb81193f986e8fae4a46a513
petrashovdaniill-dotcom <petrashov.daniill@gmail.com>
2026-07-22 17:19:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -165,7 +165,7 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "🚗 R14 — 1000₽\n"
             "🚙 R15-R16 — 1500₽\n"
             "🚘 R17-R18 — 2000₽\n\n"
-            "Балансировка входит в стоимость."
+            "Балансировка входит в стоимось."
         )
 
         context.user_data["info_msg"] = msg.message_id
